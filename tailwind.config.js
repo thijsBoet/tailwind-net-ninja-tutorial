527067a154a9dd3fac0aa1f5623306fc24aba619
thijsBoet <m.boet@chello.nl>
2022-05-07 15:28:46 +0200

    added custom extended styling in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-	content: ['./src/**/*.{html,js}',
-	'./public/**/*.{html,js}'],
+	content: ['./src/**/*.{html,js}', './public/**/*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#FF6363',
+				'secondary': {
+					100: '#E2E2D5',
+					200: '#888883',
+				}
+			},
+		},
 	},
 	plugins: [],
 };
